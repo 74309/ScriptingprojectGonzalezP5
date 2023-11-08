887100edff2c52c148a48e0123f11545ef4611c2
74309 <743095@student.dusd.net>
2023-11-08 12:44:23 -0800

    Loops
    Did Loops scripting assinment

---
+++ ScriptingProjectP5Gonzalez/Assets/Scripts/WhileLoop.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileLoop : MonoBehaviour
+{
+    int cupInTheSink = 4;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        while(cupInTheSink > 0)
+        {
+            Debug.Log("I've washed a cup!");
+            cupInTheSink--;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
